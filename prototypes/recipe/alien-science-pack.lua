808e36f12abb719a7c089ac12c42894659733251
bdean20 <benjamin.dean@hotmail.com>
2017-02-18 19:23:27 +1000

    Less extreme alien science pack recipe buff
    Undo changes to bob's alien science pack recipes - they're not used in hard enough research to need a buff.
    Regular alien science may still be exceedingly tedious with SpaceX, but better balanced for all other scenarios.

--- prototypes/recipe/alien-science-pack.lua
+++ prototypes/recipe/alien-science-pack.lua
@@ -1,21 +1,3 @@
 if (data.raw.recipe["alien-science-pack"] ~= nil) then
-	data.raw.recipe["alien-science-pack"].result_count = 1000
-end
-if (data.raw.recipe["alien-science-pack-red"] ~= nil) then
-	data.raw.recipe["alien-science-pack-red"].result_count = 1000
-end
-if (data.raw.recipe["alien-science-pack-green"] ~= nil) then
-	data.raw.recipe["alien-science-pack-green"].result_count = 1000
-end
-if (data.raw.recipe["alien-science-pack-yellow"] ~= nil) then
-	data.raw.recipe["alien-science-pack-yellow"].result_count = 1000
-end
-if (data.raw.recipe["alien-science-pack-purple"] ~= nil) then
-	data.raw.recipe["alien-science-pack-purple"].result_count = 1000
-end
-if (data.raw.recipe["alien-science-pack-orange"] ~= nil) then
-	data.raw.recipe["alien-science-pack-orange"].result_count = 1000
-end
-if (data.raw.recipe["alien-science-pack-blue"] ~= nil) then
-	data.raw.recipe["alien-science-pack-blue"].result_count = 1000
+	data.raw.recipe["alien-science-pack"].result_count = 100
 end
